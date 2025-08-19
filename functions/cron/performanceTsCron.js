86c6177112429b30c257913426b188da7f1a2816
fennic <quangnq@avada.io>
2025-08-19 14:06:56 +0700

    add lam ln

--- functions/cron/performanceTsCron.js
+++ functions/cron/performanceTsCron.js
@@ -1,66 +1,19 @@
-const cron = require('node-cron');
-const { getTrelloNotification } = require('../controllers/trelloNotificationController');
-
-// Chạy vào 0h, 4h, 8h, 12h, 16h, 18h, 20h mỗi ngày
-const schedule = '28 14 * * *';
-
-
-const getShiftInfo = () => {
-    const now = new Date();
-    // Add 7 hours to compensate for timezone difference
-    // now.setHours(now.getHours() + 7);
-    const date = now.toISOString().split('T')[0];
-    const adjustedHour = now.getHours();
-    console.log('shiftTime', adjustedHour);
-    
-    // Xác định ca dựa trên giờ đã điều chỉnh
-    let shift;
-    switch(adjustedHour) {
-        case 0:
-            shift = '1';
-            break;
-        case 4:
-            shift = '2';
-            break;
-        case 8:
-            shift = '3';
-            break;
-        case 12:
-            shift = '4';
-            break;
-        case 16:
-            shift = '5.1';
-            break;
-        case 18:
-            shift = '5.2';
-            break;
-        case 20:
-            shift = '6';
-            break;
-        default:
-            shift = 'Unknown';
-    }
-
-    return { date, shift };
-};
+const cron = require('node-cron');  
+const { getWorkShiftByDateRange } = require('../services/workShiftService');
+const schedule = '"*/30 * * * *"';
 
 const startPerformanceTsCron = () => {
-    console.log('Starting Slack notification cron job...');
-    
+    console.log('Starting Performance TS cron job...');
     cron.schedule(schedule, async () => {
         try {
-            const { date, shift } = getShiftInfo();
-            console.log(date, shift);
-            
-            console.log(`Running Slack notification cron job for ${shift} on ${date}...`);
-            await getTrelloNotification(date, shift);
-            console.log(`Slack notification sent successfully for ${shift}`);
+            console.log('Performance TS cron job running...');
+            const workShifts = await getWorkShiftByDateRange(date, formattedHour);
+            console.log('workShifts', workShifts);
         } catch (error) {
-            console.error('Error in Slack notification cron job:', error);
+            console.error('Error in Performance TS cron job:', error);
         }
     });
-
-    console.log(`Slack notification cron job scheduled to run at ${schedule}`);
+    console.log(`Performance TS cron job scheduled to run at ${schedule}`);
 };
 
 module.exports = {
